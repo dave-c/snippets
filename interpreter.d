222d398f4282b072f7cdd0517eb4ded1fad85009
dominicjones <dominic.jones@gmx.co.uk>
2013-12-06 18:01:45 +0000

    Update interpreter.d

--- interpreter.d
+++ interpreter.d
@@ -5,6 +5,7 @@ import std.regex;
 import std.conv;
 import std.uni;
 import std.ascii;
+import std.algorithm;
 
 /+
  `scan` returns the next character
@@ -12,6 +13,16 @@ import std.ascii;
  `parse` generates the abstract syntax tree
  `interpret` evaluates the abstract syntax tree
  `generate` writes the abstract syntax tree to x86-64 assembler for yasm
+
+  Tokenise, parse and interpret or generate operations have their own grammatical rules.
+
+  The tokeniser needs a rule for every specific identifier it is to recognise.
+  These rules can be constructed from finite-state machines or regular expressions,
+  e.g. a named variable must start with [a-zA-Z] and may be followed by [a-zA-Z0-9],
+       a string must start and end with '"'
+  i.e. a recognised intentifier (a token) has a type and name.
+
+  http://parsingintro.sourceforge.net/
  +/
 
 enum Mode { scan, tokenise, parse, interpret, generate, }
@@ -41,10 +52,13 @@ void main(string[] args)
   foreach (input; inputs)
     {
       auto scanner = Scanner(input);
-      // scanner.evaluate();
+      scanner.evaluate();
 
       auto lexer = Lexer(scanner);
       lexer.evaluate();
+
+      auto parser = Parser(lexer);
+      parser.evaluate();
     }
 }
 
@@ -110,31 +124,9 @@ struct Scanner
 }
 
 
-string graphicalChar(char e)
-{
-  switch (e)
-    {
-    case ' ': return "space";
-    case '\n': return "newline";
-    case '\t': return "tab";
-    default: return to!string(e);
-    }
-}
-
-
-struct SpecialToken
-{
-  string[] keyword = ["import", "return"];
-  string[] primative = ["void", "int"];
-  char[] whitespace = ['\n'];
-  string[] symbol = ["=", "!", "(", ")"];
-}
-
-
 struct Token
 {
-  enum Type { unrecognised, keyword, primative, whitespace, symbol, identifier, }
-  Type type = Type.unrecognised;
+  string type = "";
   string name = "";
   int offset = 0;
   int length = 0;
@@ -142,27 +134,30 @@ struct Token
 
 struct IdentifierToken
 {
-  bool hasFirstChar(char e)
-  {
-    return std.ascii.isAlpha(e);
-  }
+  bool hasFirstChar(char e) { return std.ascii.isAlpha(e); }
+  bool hasNextChar(char e) { return std.ascii.isAlphaNum(e); }
+  string[] keyword = ["import", "return"];
+  string[] primative = ["void", "int", "string"];
+}
 
-  bool hasNextChar(char e)
-  {
-    return std.ascii.isAlphaNum(e);
-  }
+struct SymbolToken
+{
+  bool isUnary(char e) { return find(unary, e).length > 0; }
+  bool isBinary(char e, char ep) { return find(binary, [e, ep]).length > 0; }
+  char[] unary = ['=', '!', '(', ')', '[', ']', '{', '}', ';', '.', ',', '+', '-', '*', '/'];
+  string[] binary = ["==", "!="];
 }
 
 struct NumberToken
 {
+  bool hasFirstChar(char e) { return std.ascii.isDigit(e); }
+  bool hasNextChar(char e) { return hasFirstChar(e); }
 }
 
 struct StringToken
 {
-}
-
-struct WhitespaceToken
-{
+  bool hasFirstChar(char e) { return e == '"'; }
+  bool hasLastChar(char e) { return hasFirstChar(e); }
 }
 
 
@@ -187,40 +182,96 @@ struct Lexer
   bool getNext(out Token token)
   {
     bool success = false;
-
-    char e;
-    token.offset = this.scanner.offset;
-    token.length = 1;
+    char e, ep;
 
     while (this.scanner.getNext(e))
       {
-        success = true;
-
-        if (IdentifierToken().hasFirstChar(e))
+        // ignore whitespace
+        while (std.ascii.isWhite(e))
           {
-            char ep;
-            while (this.scanner.peekNext(ep) && IdentifierToken().hasNextChar(ep))
+            this.scanner.getNext(e);
+          }
+
+        // ignore comments
+        while (e == '/' && this.scanner.peekNext(ep) && ep == '*')
+          {
+            this.scanner.increment();
+            while (this.scanner.getNext(e))
+              {
+                if (e == '*' && this.scanner.peekNext(ep) && ep == '/')
+                  {
+                    this.scanner.increment();
+                    break;
+                  }
+              }
+          }
+
+        auto identifierToken = IdentifierToken();
+        if (identifierToken.hasFirstChar(e))
+          {
+            token.offset = this.scanner.offset - 1;
+            token.length = 1;
+            while (this.scanner.peekNext(ep) && identifierToken.hasNextChar(ep))
               {
                 this.scanner.increment();
                 ++token.length;
               }
-            token.type = Token.Type.identifier;
+            token.type = typeof(identifierToken).stringof;
             token.name = this.scanner.text[token.offset..token.offset + token.length];
-            break;
+            return true;
           }
 
-        else
+        auto numberToken = NumberToken();
+        if (numberToken.hasFirstChar(e))
           {
-            token.type = Token.Type.unrecognised;
-            if (std.ascii.isGraphical(e))
+            token.offset = this.scanner.offset - 1;
+            token.length = 1;
+            while (this.scanner.peekNext(ep) && numberToken.hasNextChar(ep))
               {
-                token.name =  "'" ~ to!string(e) ~ "'";
+                this.scanner.increment();
+                ++token.length;
               }
-            else
+            token.type = typeof(numberToken).stringof;
+            token.name = this.scanner.text[token.offset..token.offset + token.length];
+            return true;
+          }
+
+        auto stringToken = StringToken();
+        if (stringToken.hasFirstChar(e))
+          {
+            token.offset = this.scanner.offset - 1;
+            token.length = 1;
+            while (this.scanner.getNext(e))
               {
-                token.name = graphicalChar(e);
+                ++token.length;
+                if (this.scanner.peekNext(ep) && stringToken.hasLastChar(ep))
+                  {
+                    this.scanner.increment();
+                    ++token.length;
+                    token.type = typeof(stringToken).stringof;
+                    token.name = this.scanner.text[token.offset..token.offset + token.length];
+                    return true;
+                  }
               }
-            break;
+          }
+
+        auto symbolToken = SymbolToken();
+        if (this.scanner.peekNext(ep) && symbolToken.isBinary(e, ep))
+          {
+            this.scanner.increment();
+            token.length = 2;
+            token.type = typeof(symbolToken).stringof;
+          }
+        else if (symbolToken.isUnary(e))
+          {
+            token.length = 1;
+            token.type = typeof(symbolToken).stringof;
+          }
+        if (token.type == typeof(symbolToken).stringof)
+          {
+            token.offset = this.scanner.offset - token.length;
+            token.name = this.scanner.text[token.offset..token.offset + token.length];
+            return true;
           }
       }
 
@@ -228,4 +279,58 @@ struct Lexer
   }
 
   Scanner scanner;
+}
+
+
+struct Node
+{
+  int level = 0;
+  Token token;
+  Token[] children;
+}
+
+struct Terminal
+{
+  char opAssign = '=';
+}
+
+struct Parser
+{
+  this(ref Lexer lexer)
+  {
+    this.lexer = lexer;
+    this.lexer.scanner.clear();
+  }
+
+  void evaluate()
+  {
+    writeln("Running the parser...");
+    Node node;
+    buildTree(node);
+  }
+
+  void buildTree(out Node node)
+  {
+    // statement(node);
+  }
+
+  // void statement(out Node node)
+  // {
+  //   Token t0, t1;
+  //   if (lexer.getNext(t0) && token.type == IndentifierToken.stringof)
+  //     {
+  //       lexer.getNext(t1);
+  //       if (expect(t1, Terminal().opAssign))
+  //         {
+  //           expression(t1);
+  //         }
+  //     }
+  // }
+
+  // bool expect(ref Token token, char e)
+  // {
+  //   return token.name == e;
+  // }
+
+  Lexer lexer;
 }
