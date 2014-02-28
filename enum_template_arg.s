2774e0ab11970b53f3ea6654e0f1f113f13f8df4
Dominic Jones <dominic.jones@gmx.co.uk>
2014-02-28 14:36:46 +0000

    Create enum_template_arg.s

---
+++ enum_template_arg.s
@@ -0,0 +1,35 @@
+	.file	"enum_template_arg.cpp"
+	.text
+.globl main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	.cfi_personality 0x3,__gxx_personality_v0
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$40, %rsp
+	movl	$0, -4(%rbp)
+	leaq	-160(%rbp), %rdx
+	movl	$0, %eax
+	movl	$18, %ecx
+	movq	%rdx, %rdi
+	rep stosq
+	movq	%rdi, %rdx
+	movl	%eax, (%rdx)
+	addq	$4, %rdx
+	movl	$37, -160(%rbp)
+	movl	-160(%rbp), %eax
+	addl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.4.7 20120313 (Red Hat 4.4.7-3)"
+	.section	.note.GNU-stack,"",@progbits
